7ba8330d0e07e621308a5723f322bcee44b3a318
Peter Kemp <peterkemp@gmail.com>
2022-05-20 10:36:34 -0700

    Update index.js

--- nextjs-starter/pages/index.js
+++ nextjs-starter/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App this is a test</title>
+        <title>Create Next App this is a test or is it</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
